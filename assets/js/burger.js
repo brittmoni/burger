56469bc7f929899ab188d50908f575f5f3335167
Brittany F <brittfranklin92@gmail.com>
2019-06-08 10:01:54 -0400

    JS updates

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,1 +1,12 @@
-$()
+$('.add-burger').on('submit', function(event) {
+  event.preventDefault();
+  
+  $.ajax('/api/burgers', {
+    type: 'POST',
+    data: newBurger
+  }).then(
+    function() {
+      console.log('New burger added');
+    }
+  )
+})
